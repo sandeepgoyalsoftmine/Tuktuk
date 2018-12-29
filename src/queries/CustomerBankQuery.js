aaf6e07988489eddc88b2fee07ab087a238a723a
Amit <singla.amit0289@gmail.com>
2018-12-29 18:04:15 +0530

    increase size of bank name

--- src/queries/CustomerBankQuery.js
+++ src/queries/CustomerBankQuery.js
@@ -1,5 +1,5 @@
 export  const FETCH_BANK_DETAILS_WITH_DRIVER = `
-SELECT bankid,bank_name, account_holder, account, ifsc_code, DATE_FORMAT(tbbank.created_on, '%Y-%m-%d') AS date 
+SELECT bankid,bank_name, account_holder, account, ifsc_code, DATE_FORMAT(tbcustomerbank.created_on, '%Y-%m-%d') AS date 
 , tbusers.name, tbusers.emailid
 FROM tbcustomerbank
 LEFT OUTER JOIN tbusers on tbcustomerbank.driver_id = tbusers.userid
