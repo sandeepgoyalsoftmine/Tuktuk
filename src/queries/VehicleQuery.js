e9fe4ff216df704508258d4fcd1eeee738929236
Amit <singla.amit0289@gmail.com>
2019-01-05 14:25:50 +0530

    set false if any bankdetails is missing for edit bank for api

--- src/queries/VehicleQuery.js
+++ src/queries/VehicleQuery.js
@@ -1,6 +1,6 @@
 export const FETCH_ALL_VEHICLE = `
 SELECT tbvehicle.vehicle_id, tbvehicletypes.vehicle_type, make, model, vehicle_number, rc_no, permit_no, insurance_no, 
-DATE_FORMAT(tbvehicle.created_on, '%Y-%m-%d %H:%i:%s') AS created_on, tbvehicle.status, tbusers.name
+DATE_FORMAT(tbvehicle.created_on, '%Y-%m-%d %H:%i:%s') AS created_on, tbvehicle.status, tbusers.name,tbusers.mobile_no
 FROM tbvehicle
 LEFT OUTER JOIN tbvehicletypes on tbvehicle.vehicle_type = tbvehicletypes.vehicle_id
 LEFT OUTER JOIN tbusers on tbvehicle.assigned_driver_id = tbusers.userid
