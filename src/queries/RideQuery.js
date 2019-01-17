0a3045a55b911f4585aa6629ba86f8eb54918f54
amitsingla0289 <singla.amit0289@gmail.com>
2019-01-17 17:17:36 +0530

    update error code in checktoken

--- src/queries/RideQuery.js
+++ src/queries/RideQuery.js
@@ -3,7 +3,6 @@ SELECT id
 from tb_ride_details
 WHERE id=:ride_id 
 `;
-
 export const FETCH_ALL_TODAYS_RIDE = `
 SELECT concat(tbusers.name ,' (', tbusers.mobile_no,')') as driver_name, 
 concat(tbcustomers.name,' (',tbcustomers.mobile_no,')')  as customer_name,tb_ride_details.source_lat, tb_ride_details.source_long, 
